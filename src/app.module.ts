ac8250e68258e525153796f72e13f3e7d1f1e777
Mustafa-Alhasanat <Mustafa.AlHasanat@optimumpartners.co>
2023-09-11 15:21:19 +0300

    tables modifications

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,8 @@ import { UsersModule } from './schemas/users/users.module';
 import { AuthModule } from './schemas/auth/auth.module';
 import entities from './entities';
 import { JwtModule } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './schemas/auth/auth.guard';
+// import { APP_GUARD } from '@nestjs/core';
+// import { AuthGuard } from './schemas/auth/auth.guard';
 import { OrdersModule } from './schemas/orders/orders.module';
 import { ReviewsModule } from './schemas/reviews/review.module';
 
@@ -48,10 +48,10 @@ import { ReviewsModule } from './schemas/reviews/review.module';
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: AuthGuard,
+    // },
   ],
 })
 export class AppModule {}
